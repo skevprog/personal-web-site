964d7513121f49cb9a9f89c6262117a09d21e36a
Kevin Soruco <kevin-s93@hotmail.com>
2023-01-29 20:43:18 -0300

    style
    * Refactor styles for mobile navBar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
-import { routeNames, socialIconLinks } from './routes';
+import { routeNames } from './routes';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 function Navbar() {
@@ -30,7 +30,7 @@ function Navbar() {
                <ul className="hidden md:flex">
                   {routeNames.map(route => (
                      <Link href={route.href} key={route.id}>
-                        <li className="ml-10 text-sm text-center uppercase rounded p-2 hover:scale-105 hover:font-bold ease-in duration-100 w-24">
+                        <li className="ml-16 text-md text-center rounded p-2 hover:scale-105 hover:font-bold ease-in duration-100 w-24 justify-center">
                            <span>{route.label}</span>
                         </li>
                      </Link>
@@ -51,55 +51,32 @@ function Navbar() {
                <div
                   className={
                      nav
-                        ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#4e4a59] p-10 ease-in duration-300'
+                        ? 'fixed left-0 top-0 w-[100%] sm:w-[60%] md:w-[45%] h-screen h-[100%] bg-[#4e4a59] p-10 ease-in duration-300'
                         : 'fixed left-[-100%] top-0 p-10 ease-in duration-300'
                   }
                >
-                  <div className="flex w-full items-center justify-between">
+                  <div className="flex w-full justify-end">
                      <button
-                        className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+                        className="rounded-full p-3 cursor-pointer"
                         onClick={handleNav}
                      >
                         <AiOutlineClose />
                      </button>
                   </div>
-                  <div className="border-b border-gray-300 my-4">
-                     <p className="w-[85%] md:w-[90%] py-4">
-                        Let&apos;s build something legendary together
-                     </p>
-                  </div>
-                  <div>
-                     <ul className="py-4 flex-col">
+                  <div className="flex justify-center">
+                     <ul className="mt-[30%]">
                         {routeNames.map(route => (
                            <Link
                               href={route.href}
                               key={route.id}
                               onClick={handleNav}
                            >
-                              <li className="py-4 uppercase text-sm">
+                              <li className="py-4 mb-12 uppercase text-lg justify-center">
                                  <span>{route.label}</span>
                               </li>
                            </Link>
                         ))}
                      </ul>
-                     <div className="pt-40">
-                        <p className="uppercase tracking-widest text-[#5651e5]">
-                           Let&apos;s Connect
-                        </p>
-                        <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
-                           {socialIconLinks.map(link => {
-                              const Icon = link.icon;
-                              return (
-                                 <div
-                                    className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300"
-                                    key={link.id}
-                                 >
-                                    <Icon />
-                                 </div>
-                              );
-                           })}
-                        </div>
-                     </div>
                   </div>
                </div>
             </div>
