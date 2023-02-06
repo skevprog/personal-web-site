68cdb06108a9bf910f6a813d9a6fc38d17dcc047
Kevin Soruco <kevin-s93@hotmail.com>
2023-02-06 12:30:19 -0300

    feat
    * Add animations to the navBar component (mobile & Desktop)

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,14 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { routeNames } from './routes';
+import { AnimatePresence, motion, useCycle } from 'framer-motion';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import useScrollspy from '../../hooks/useScrollSpy';
+
+const itemVariants = {
+   closed: {
+      opacity: 0,
+   },
+   open: { opacity: 1 },
+};
+
+const sideVariants = {
+   closed: {
+      transition: {
+         staggerChildren: 0.1,
+         staggerDirection: -1,
+      },
+   },
+   open: {
+      transition: {
+         staggerChildren: 0.2,
+         staggerDirection: 1,
+      },
+   },
+};
+
+const ids = ['home', 'about', 'contact'];
 
 function Navbar() {
-   const [nav, setNav] = useState(false);
+   const [open, cycleOpen] = useCycle(false, true);
    const [shadow, setShadow] = useState(false);
+   const activeId = useScrollspy(ids, 80);
+
+   const router = useRouter();
 
    const handleScroll = () => setShadow(window.scrollY >= 90);
+   const isSelectedPath = (path: string) => router.asPath === `/#${path}`;
 
    useEffect(() => {
       window.addEventListener('scroll', handleScroll);
@@ -16,8 +47,6 @@ function Navbar() {
          window.removeEventListener('scroll', handleScroll);
       };
    }, []);
-
-   const handleNav = () => setNav(!nav);
 
    return (
       <div
@@ -29,56 +58,118 @@ function Navbar() {
             <div>
                <ul className="hidden md:flex">
                   {routeNames.map(route => (
-                     <Link href={route.href} key={route.id}>
-                        <li className="ml-10 text-md text-center rounded p-2 hover:scale-105 hover:font-bold ease-in duration-100 w-24 justify-center">
-                           <span>{route.label}</span>
-                        </li>
-                     </Link>
+                     <li
+                        className={`ml-10 text-md text-center rounded p-2 w-24 justify-center ${
+                           isSelectedPath(route.id) ? 'font-bold' : ''
+                        }`}
+                        key={route.id}
+                     >
+                        <Link href={route.href}>
+                           <motion.div whileHover={{ scale: 1.2 }}>
+                              <span>{route.label}</span>
+                              <AnimatePresence>
+                                 {activeId === route.id && (
+                                    <motion.div
+                                       animate={{ rotate: 180, scale: 1 }}
+                                       className="underline"
+                                       exit={{
+                                          scale: 0,
+                                          rotate: -180,
+                                          transition: { duration: 0.2 },
+                                       }}
+                                       initial={{ scale: 0 }}
+                                       transition={{
+                                          type: 'spring',
+                                          stiffness: 260,
+                                          damping: 20,
+                                       }}
+                                    />
+                                 )}
+                              </AnimatePresence>
+                           </motion.div>
+                        </Link>
+                     </li>
                   ))}
                </ul>
-               <div className="md:hidden cursor-pointer" onClick={handleNav}>
+               <div
+                  className="md:hidden cursor-pointer"
+                  onClick={() => cycleOpen()}
+               >
                   <AiOutlineMenu size={25} />
                </div>
             </div>
 
             <div
                className={
-                  nav
+                  open
                      ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-[#394053]/60'
                      : ''
                }
             >
-               <div
-                  className={
-                     nav
-                        ? 'fixed left-0 top-0 w-[100%] sm:w-[60%] md:w-[45%] h-screen h-[100%] bg-[#4e4a59] p-10 ease-in duration-300'
-                        : 'fixed left-[-100%] top-0 p-10 ease-in duration-300'
-                  }
-               >
-                  <div className="flex w-full justify-end">
-                     <button
-                        className="rounded-full p-3 cursor-pointer"
-                        onClick={handleNav}
+               <AnimatePresence>
+                  {open && (
+                     <motion.aside
+                        animate={{
+                           width: '100%',
+                        }}
+                        className="mobile-navbar"
+                        exit={{
+                           width: 0,
+                           transition: { delay: 0.6, duration: 0.2 },
+                        }}
+                        initial={{ width: 0 }}
                      >
-                        <AiOutlineClose />
-                     </button>
-                  </div>
-                  <div className="flex justify-center">
-                     <ul className="mt-[30%]">
-                        {routeNames.map(route => (
-                           <Link
-                              href={route.href}
-                              key={route.id}
-                              onClick={handleNav}
+                        <motion.div className="flex w-full justify-end p-10">
+                           <motion.button
+                              animate="open"
+                              className="rounded-full p-3 cursor-pointer"
+                              exit="closed"
+                              onClick={() => cycleOpen()}
+                              whileTap={{ scale: 0.5 }}
                            >
-                              <li className="py-4 mb-12 uppercase text-lg justify-center">
-                                 <span>{route.label}</span>
+                              <AiOutlineClose />
+                           </motion.button>
+                        </motion.div>
+                        <motion.ul
+                           animate="open"
+                           className="container text-center"
+                           exit="closed"
+                           initial="closed"
+                           variants={sideVariants}
+                        >
+                           {routeNames.map(({ label, id, href }) => (
+                              <li className="justify-center" key={id}>
+                                 <Link href={href} onClick={() => cycleOpen()}>
+                                    <motion.span
+                                       variants={itemVariants}
+                                       whileHover={{ scale: 1.1 }}
+                                    >
+                                       {label}
+                                    </motion.span>
+                                    {isSelectedPath(id) && (
+                                       <motion.div
+                                          animate={{ rotate: 180, scale: 1 }}
+                                          className="underline"
+                                          exit={{
+                                             scale: 0,
+                                             rotate: -180,
+                                             transition: { duration: 0.2 },
+                                          }}
+                                          initial={{ scale: 0 }}
+                                          transition={{
+                                             type: 'spring',
+                                             stiffness: 260,
+                                             damping: 20,
+                                          }}
+                                       />
+                                    )}
+                                 </Link>
                               </li>
-                           </Link>
-                        ))}
-                     </ul>
-                  </div>
-               </div>
+                           ))}
+                        </motion.ul>
+                     </motion.aside>
+                  )}
+               </AnimatePresence>
             </div>
          </div>
       </div>
